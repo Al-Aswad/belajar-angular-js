05e30ec1546577c2c07dc50a3326c1e037b2fde6
Aswad Home <hajaraswadkom@gmail.com>
2021-09-22 06:34:10 +0800

    firts

---
+++ js/mainController.js
@@ -0,0 +1,43 @@
+app.controller('myCtrl', ['$scope',
+    function($scope) {
+        $scope.title = 'Belajar Angular JS';
+        $scope.books = [
+
+            {
+                title: 'belajar Angular Js',
+                author: 'Agung Setiawan',
+                price: '5000',
+                pubdate: new Date(2021, 9 - 1, 22),
+                likes: 0
+            },
+            {
+                title: 'belajar Angular Js',
+                author: 'Agung Setiawan',
+                price: '5000',
+                pubdate: new Date(2021, 9 - 1, 22),
+                likes: 0
+            },
+            {
+                title: 'belajar Angular Js',
+                author: 'Agung Setiawan',
+                price: '5000',
+                pubdate: new Date(2021, 9 - 1, 22),
+                likes: 0
+            },
+            {
+                title: 'belajar Angular Js',
+                author: 'Agung Setiawan',
+                price: '5000',
+                pubdate: new Date(2021, 9 - 1, 22),
+                likes: 0
+            }
+        ];
+        $scope.logToConsole = function(index) {
+            let book = $scope.books[index];
+            console.log(book);
+        }
+        $scope.likes = function(index) {
+            $scope.books[index].likes += 1;
+        }
+    }
+]);
